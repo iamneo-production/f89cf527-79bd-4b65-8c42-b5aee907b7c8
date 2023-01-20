6132e99a8db1b7cd0a8e93847faef85000eb9fa6
chavinco <tshabalala.nicolas@gmail.com>
2023-01-20 14:45:50 +0000

    Simiso display user function

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { SearchComponent } from './search/search.component';
+import { DisplayUserComponent } from './display-user/display-user.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SearchComponent,
+    DisplayUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    Ng2SearchPipeModule,
     ReactiveFormsModule,
     NgbModule
   ],
