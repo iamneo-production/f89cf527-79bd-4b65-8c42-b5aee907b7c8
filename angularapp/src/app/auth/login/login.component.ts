20a77a2a7a21e4a58b2d19b222b8882443319fe1
chavinco <tshabalala.nicolas@gmail.com>
2023-01-23 13:33:50 +0000

    login changes

---
+++ angularapp/src/app/auth/login/login.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login/login.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loggedUser:any;
+
+  constructor(private loginService: LoginService, private router: Router) { }
+  
+  ngOnInit(): void {
+   console.log("OnInit");
+   
+  }
+
+
+  loginForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(12)]),
+
+    }
+  )
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  loginUser(loginForm: any) {
+
+    this.loginForm.value ?
+    this.loginService.login(this.loginForm.value.username, this.loginForm.value.password)
+      .subscribe(
+        (next: any) => {
+          this.loggedUser = next[0];
+
+          if (this.loggedUser != undefined) {
+            localStorage.setItem('loggedUser', JSON.stringify(this.loggedUser));
+            let user: any = localStorage.getItem('loggedUser');
+            console.log(JSON.parse(user));
+            this.router.navigate(['/'])
+          } else {
+            alert('Login Failed')
+          }
+         
+
+        }
+      ) :
+    alert("Invalid login form")
+  }
+
+  
+}
+
+
