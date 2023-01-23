6b66c2170f8ade0115cea5f1fd97746542447f8a
chavinco <tshabalala.nicolas@gmail.com>
2023-01-23 10:16:47 +0000

    Navigation and Auth commit

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,32 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { SearchComponent } from './search/search.component';
-import { DisplayUserComponent } from './display-user/display-user.component';
-
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SearchComponent,
-    DisplayUserComponent
+    SignupComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule,
-    Ng2SearchPipeModule,
-    ReactiveFormsModule,
-    NgbModule
+    MatButtonModule,MatToolbarModule,MatCardModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
