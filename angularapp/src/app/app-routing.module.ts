7e5cf12ac1a1ce079f90a1266defd2184050e2fa
chavinco <tshabalala.nicolas@gmail.com>
2023-01-25 13:27:54 +0000

    Home page commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminhomepageComponent } from './adminhomepage/adminhomepage.component';
-import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuardGuard } from './guards/auth-guard.guard';
 import { HomepageComponent } from './homepage/homepage.component';
 import { ViewCartComponent } from './view-cart/view-cart.component';
+import { ViewOrderComponent } from './view-order/view-order.component';
 import { ViewProductComponent } from './view-product/view-product.component';
 
 
@@ -23,9 +23,15 @@ const routes: Routes = [
         canActivate:[AuthGuardGuard]
       },
       {
-        path:'cart',
-        component:ViewCartComponent
-      }
+        path:'carts',
+        component:ViewCartComponent,
+        canActivate:[AuthGuardGuard]
+      },
+      {
+        path:'orders',
+        component:ViewOrderComponent,
+        canActivate:[AuthGuardGuard]
+      },
     ]
   },
   
---
+++ angularapp/src/app/services/delete-cart.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeleteCartService {
+  
+  url = "https://8080-cafcfccaceabeffbedeadcfdbdaabaca.examlyiopb.examly.io/carts";
+
+  constructor(private http:HttpClient) { }
+  
+  deleteCart(data:any)
+  {
+    return this.http.delete(this.url+"/"+data.id);
+  }
+}
