8dfbbe076dcadd8acdf29a269ea2ea067c3e03bc
simisophili <simisophili@gmail.com>
2023-01-27 06:59:57 +0000

    Image link to Info Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -9,56 +9,32 @@ import { ViewProductService } from './services/view-product.service';
 })
 export class AppComponent {
   products:any;
-<<<<<<< HEAD
-
-  
   isAdmin:boolean = false;
   loggedIn:boolean = false;
 
-=======
-
-  
-  // isAdmin:boolean = false;
-  // loggedIn:boolean = false;
-
->>>>>>> 86893bcee15d306a190eab5e3620c337a5a9a668
   constructor(private router:Router,private viewproductService:ViewProductService){
     viewproductService.products().subscribe((data:any)=>
     {
       console.log(data);
       this.products=data;
     });
-<<<<<<< HEAD
   
-  //   let s = localStorage.getItem('loggedUser');
-  //   if(s != null){
-  //     this.isAdmin = JSON.parse(s).role == "admin" ? true : false;
-  //     this.loggedIn = true;
-  //   }
+    let s = localStorage.getItem('loggedUser');
+    if(s != null){
+      this.isAdmin = JSON.parse(s).role == "admin" ? true : false;
+      this.loggedIn = true;
+    }
      
-  //   console.log(s);
-  // }
+    console.log(s);
+  }
  
-=======
-  }
-//     let s = localStorage.getItem('loggedUser');
-//     if(s != null){
-//       this.isAdmin = JSON.parse(s).role == "admin" ? true : false;
-//       this.loggedIn = true;
-//     }
-     
-//     console.log(s);
-    
-//  }
->>>>>>> 86893bcee15d306a190eab5e3620c337a5a9a668
   
-  }
+  
   logOut(){
     localStorage.clear()
     this.router.navigate(['login'])
-  //Might have to comment
-    // window.location.reload();
+  
+  
   }
-  }
-
+}
 
--- angularapp/src/app/product-info/product-info.component.ts
+++ angularapp/src/app/product-info/product-info.component.ts
@@ -9,11 +9,41 @@ import { ViewProductService } from '../services/view-product.service';
 export class ProductInfoComponent{
 
   products:any;
+  singleproduct:any={};
+  Id:any;
+  productId:any=0;
+
   constructor(private viewproductService: ViewProductService) {
     viewproductService.products().subscribe((data:any)=>
     {
       this.products=data;
     })
+   }
+
+
+
+
+  //  Simiso view
+   searchProduct(id){
+     this.productId=this.Id;
+     this.getReviews(this.Id);
+   }
+
+
+
+
+
+
+   public getReviews(id){
+     const searchValue=id;
+     this.viewproductService.products().subscribe((data)=>{
+       this.products=data;
+       this.products.map((product, index)=>{
+         if (product.id===searchValue){
+           this.products=product;
+         }
+       })
+     })
    }
 
 
